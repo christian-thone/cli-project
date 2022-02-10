649b89db646fbc77a124f1990d99a7f227192a2d
Christian Thone <christiandthone@gmail.com>
2022-02-09 22:02:17 -0500

    First Upload

---
+++ cli.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+//* Reserved space for concat function.
+
+
+int main()
+{
+    const string games[1] = {
+        "Rock, Paper, Scissors"
+    };
+    cout << "Choose a game:" << endl;
+
+    for (int i=0; i < games->size(); i++)
+    {
+        // ToDo: Write a function to make concats easier
+    }
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Under Construction
+There is no license, but be warned there will be bugs and/or unfinished features.
