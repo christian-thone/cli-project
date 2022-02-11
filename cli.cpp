a262c76fbf53685d6ef8ecabe6e24a13def38061
Christian Thone <christiandthone@gmail.com>
2022-02-10 20:39:58 -0500

    Finished v1 of rps

--- cli.cpp
+++ cli.cpp
@@ -4,23 +4,26 @@
 
 using namespace std;
 
-int main() {
-  const string games[4] = {
-    "Rock Paper Scissors (rps)",
-    "Guess the Number (gtn)",
-    "Guess the Word (gtw)",
-    "Unallocated (exit)"
+int main()
+{
+  const string games[2] = {
+      "Rock Paper Scissors (rps)",
+      "Random Game"
   };
 
-  cout << "Welcome to C++ Games!\n" << endl;
+  cout << "Welcome to C++ Games!\n"
+       << endl;
   cout << "Choose one of the following games:" << endl;
 
-  for (int i = 0; i < games->size(); i++) {
-    if (i > 3) break;
+  for (int i = 0; i < 2; i++)
+  {
+    if (i > 2)
+      break;
     cout << games[i] << endl;
   }
 
-  cout << "\n" << endl;
+  cout << "\n"
+       << endl;
 
   // Collectin user input.
   string input = "";
@@ -30,11 +33,17 @@ int main() {
   // Checking user input
 
   if (input == "--exit" || input == "--e")
-     {cout << "Exiting..." << endl; exit( 0 );} else {
+  {
+    cout << "Exiting..." << endl;
+    exit(0);
+  }
+  else
+  {
     if (input == "rps" || input == "rock paper scissors")
     {
       rockPaperScissors();
     }
   }
 
+  return 0;
 }
--- games/rps.cpp
+++ games/rps.cpp
@@ -4,27 +4,36 @@
 
 using namespace std;
 
+int doubleNewLines() {
+  cout << "\n" << endl;
+  cout << "\n" << endl;
+  return 0;
+}
+
 int rockPaperScissors()
 {
   string input;
   string choices[3] = {"r", "p", "s"};
-  cout << "\n" << endl;
-  cout << "\n" << endl;
+  doubleNewLines();
 
   cout << "Rock! Paper! Scissors!" << endl;
   cout << "Choose one letter as your play option." << endl;
   cout << "It can only be 'r', 'p', or 's'" << endl;
 
-
   while (!(input == "--exit"))
   {
     cout << "> ";
     getline(cin, input);
-    if (input == "--exit") cout << "Exiting.." <<endl; exit(0);
+    /* if (input == "--exit")
+      cout << "Exiting.." << endl;
+      exit(0); */
+      doubleNewLines();
     cout << "You chose " + input << endl;
 
-    int randomIndex; randomIndex = rand() % 4;
-    string aiChoice; aiChoice = choices[randomIndex];
+    int randomIndex;
+    randomIndex = rand() % 4;
+    string aiChoice;
+    aiChoice = choices[randomIndex];
 
     cout << "AI Chose: " + aiChoice << endl;
 
@@ -33,43 +42,48 @@ int rockPaperScissors()
       if (input == "r")
       {
         cout << "Oh! You both chose the same thing" << endl;
-      } else if (input == "s")
+      }
+      else if (input == "s")
       {
         cout << "You lost! Rock beats Scissors!" << endl;
-      } else if (input == "p")
+      }
+      else if (input == "p")
       {
         cout << "You win! Paper beats Rock!" << endl;
       }
-    } else if (aiChoice == "p")
+    }
+    else if (aiChoice == "p")
     {
       if (input == "p")
       {
         cout << "Oh! You both chose the same thing" << endl;
-      } else if (input == "r")
+      }
+      else if (input == "r")
       {
         cout << "You lost! Paper beats Rock!" << endl;
-      } else if (input == "s")
+      }
+      else if (input == "s")
       {
         cout << "You win! Scissors beats Paper" << endl;
       }
-    } else if (aiChoice == "s")
+    }
+    else if (aiChoice == "s")
     {
       if (input == "s")
       {
         cout << "Oh! You both chose the same thing" << endl;
-      } else if (input == "r")
+      }
+      else if (input == "r")
       {
         cout << "You lost! Rock beats Scissors!" << endl;
-      } else if (input == "p")
+      }
+      else if (input == "p")
       {
         cout << "You win! Scissors beats Paper" << endl;
       }
     }
 
-    cout << "\n" << endl;
-    cout << "\n" << endl;
-
-
+    doubleNewLines();
   }
 
   return 0;
